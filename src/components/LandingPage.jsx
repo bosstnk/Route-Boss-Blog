72c6caa6c7e6e007137a04bca50d3fa66db9ff13
thanakorn <thanakorn.stsb@gmail.com>
2025-12-28 18:41:50 +0700

    feat: enhance landing page with HeroSection component and integrate Google Fonts

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "./HeroSection"
+
+function LandingPage () {
+    return (
+        <div className="w-full px-[7.5rem] py-[3.75rem] bg-base-brown-100">
+            <HeroSection />
+        </div>
+    );
+}
+
+export default LandingPage;
