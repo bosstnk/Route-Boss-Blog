a8a2f15e0341d97bee3d8aa7f9cb788770f72478
thanakorn <thanakorn.stsb@gmail.com>
2025-12-28 15:54:42 +0700

    feat: add NavBar component and logo, update App component structure

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+function NavBar() {
+  return (
+    // pading x-[7.5rem] 120px or padding x-32 128px
+    <nav className="w-full px-[7.5rem] py-4 flex items-center justify-between bg-base-brown-100 border-b border-b-base-brown-300">
+      <img src="src\components\images\logo.svg" alt="logo-page" />
+
+      <div className="flex gap-8">
+        <button className="text-body-1 text-base-brown-600 bg-base-white inline-flex items-center justify-center rounded-full px-10 py-3 leading-6 border border-base-brown-400">
+          Log in
+        </button>
+
+        <button className="text-body-1 text-base-white bg-base-brown-600 inline-flex items-center justify-center rounded-full px-10 py-3 leading-6">
+          Sign up
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
