2f553087bf2247d8d2126d7c5c84cff704d538f1
thanakorn <thanakorn.stsb@gmail.com>
2026-01-15 00:27:39 +0700

    refactor: remove logo SVG and update NavBar layout and button styles

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,8 +1,8 @@
 export function Button({text,variant="primary",className="",...props}) {
-    const base = "text-body-1 leading-6 px-10 py-3 rounded-full inline-flex items-center justify-center "
+    const base = "text-body-1 leading-6 px-10 py-3 rounded-full"
     const variants = {
-        primary: `${base} text-base-white bg-base-brown-600`,
-        secondary: `${base} text-base-brown-600 bg-base-white border border-base-brown-400`,
+        primary: `${base} text-white bg-base-brown-600`,
+        secondary: `${base} text-base-brown-600 bg-white border border-base-brown-400`,
         text:"text-body-1 text-base-brown-600 underline",
         tab:"text-body-1 px-5 py-3 text-base-brown-400"
     }
